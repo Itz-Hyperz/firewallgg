7c1b4eb6a1e4afe8204d27285a5f2d4017ce1255
Itz-Hyperz <logan@hyperz.net>
2022-07-10 13:32:40 -0700

    Update main.js

--- module/main.js
+++ module/main.js
@@ -16,40 +16,59 @@ async function code(userId) {
     for(let database of databases) {
         count++
         let themeColor = database.themeColor || '#FFFFFF';
-        let request = await axios({
-            method: database.method,
-            url: `${database.requestUrl}${userId}`,
-            headers: {
-                'Authorization': `Bearer r1O.CJZ9LVtLKEs2ocpZ0sR1C3436H`
-            }
-        });
-        if(!request?.data) return;
-        let data = request.data;
+        let request;
+        try {
+            request = await axios({
+                method: database.method,
+                url: `${database.requestUrl}${userId}`,
+                headers: {
+                    'Authorization': `Bearer r1O.CJZ9LVtLKEs2ocpZ0sR1C3436H`
+                }
+            });
+        } catch(e) {
+            if(e.toString().includes('Request failed')) {
+                request = "failed";
+            };
+        };
         let _json = "STRING";
-        if(Array.isArray(data)) {
-            // Stronger Together Array Check
-            data = await data.find(o => o.active == 1);
-        };
-        if(typeof data?.time == 'number' || typeof data?.date == 'number') {
-            data.time = data?.time?.toDateString();
-            data.date = data?.date?.toDateString();
-        };
-        if(data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active) {
-            if(typeof data?.active == 'number' || typeof data?.blacklistdata?.blacklisted == 'number' || typeof data?.blacklistdata?.active == 'number') {
-                data.active = await booleanConvert(data?.active) || data?.active;
-                data.blacklistdata.blacklisted = await booleanConvert(data?.blacklistdata?.blacklisted) || data?.blacklistdata?.blacklisted;
-                data.blacklistdata.active = await booleanConvert(data?.blacklistdata?.active) || data?.blacklistdata?.active;
+        if(request != "failed") {
+            if(!request?.data) return;
+            let data = request.data;
+            if(Array.isArray(data)) {
+                // Stronger Together Array Check
+                data = await data.find(o => o.active == 1);
             };
-            _json = {
-                "database": database.name,
-                "themeColor": themeColor,
-                "active": data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active,
-                "userid": data?.userid || data?.user?.id || data?.user || 'NA',
-                "reason": data?.reason || data?.blacklistdata?.reason || data?.public_reason || 'NA',
-                "proof": data?.proof || 'None provided...',
-                "time": data?.time || data?.blacklistdata?.date || 'NA'
+            if(typeof data?.time == 'number' || typeof data?.date == 'number') {
+                if(typeof data?.time != 'undefiend') {
+                    data.time = data?.time?.toDateString();
+                };
+                if(typeof data?.date != 'undefined') {
+                    data.date = data?.date?.toDateString();
+                };
             };
-        };
+            if(data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active) {
+                if(typeof data?.active == 'number' || typeof data?.blacklistdata?.blacklisted == 'number' || typeof data?.blacklistdata?.active == 'number') {
+                    if(typeof data?.active != 'undefined') {
+                        data.active = await booleanConvert(data?.active) || data?.active;
+                    };
+                    if(typeof data?.blacklistdata?.blacklisted != 'undefined') {
+                        data.blacklistdata.blacklisted = await booleanConvert(data?.blacklistdata?.blacklisted) || data?.blacklistdata?.blacklisted;
+                    };
+                    if(typeof data?.blacklistdata?.active != 'undefined') {
+                        data.blacklistdata.active = await booleanConvert(data?.blacklistdata?.active) || data?.blacklistdata?.active;
+                    };
+                };
+                _json = {
+                    "database": database.name,
+                    "themeColor": themeColor,
+                    "active": data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active,
+                    "userid": data?.userid || data?.user?.id || data?.user || 'NA',
+                    "reason": data?.reason || data?.blacklistdata?.reason || data?.public_reason || 'NA',
+                    "proof": data?.proof || 'None provided...',
+                    "time": data?.time || data?.blacklistdata?.date || 'NA'
+                };
+            };
+        }
         if(typeof _json == 'object') {
             await bans.push(_json);
         };
