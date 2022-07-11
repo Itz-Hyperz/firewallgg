88dd9a71d60d4f9aaeb7d34c515b5abb490c6fdf
Itz-Hyperz <logan@hyperz.net>
2022-07-11 07:45:41 -0700

    Update main.js

--- module/main.js
+++ module/main.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 module.exports = async function(userId) {
     let listFetch = await axios({
         method: 'get',
-        url: `https://firewall.hyperz.net/api/checkuser/${req.params.userid}`
+        url: `https://firewall.hyperz.net/api/checkuser/${userId}`
     });
     let bannedList = listFetch.data;
     return bannedList;
