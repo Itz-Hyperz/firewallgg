09a301add669ed07135d98d74d05af764a626036
Itz-Hyperz <logan@hyperz.net>
2022-07-10 17:17:00 -0700

    Update apiExample.js

--- examples/apiExample.js
+++ examples/apiExample.js
@@ -9,7 +9,7 @@ app.get('/firewallgg/checkuser/:userid', async function(req, res) {
     res.set('Access-Control-Allow-Origin', '*');
     if(!req?.params?.userid) return res.redirect('/');
     req.params.userid == req.params.userid.replaceAll('`', '').replaceAll('"', '');
-    await con.query(`SELECT * FROM bans WHERE userid="${req?.params?.userid}"`, async (err, row) => {
+    await con.query(`SELECT * FROM bans WHERE userid="${req?.params?.userid}" AND active="true"`, async (err, row) => {
         if(err) throw err;
         if(!row[0]) {
             // If the user is not banned
@@ -24,7 +24,7 @@ app.get('/firewallgg/checkuser/:userid', async function(req, res) {
                 "userid": row[0].userid,
                 "reason": row[0].reason,
                 "proof": row[0].proof || 'None provided...',
-                "time": row[0].date
+                "time": row[0].timeofban
             }
             return res.type('json').send(JSON.stringify(json_, null, 4) + '\n');
         };
