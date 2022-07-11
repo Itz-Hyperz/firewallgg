381dcffdcaa29268348a37bb8f2854436cec2a23
Itz-Hyperz <logan@hyperz.net>
2022-07-10 22:20:46 -0700

    Code cleanup

--- module/main.js
+++ module/main.js
@@ -15,88 +15,71 @@ async function code(userId) {
     let databases = databaseRequest.data;
     for(let database of databases) {
         count++
-        if(!database.active) return;
-        let themeColor = database.themeColor || '#FFFFFF';
-        let logoUrl = database.logoUrl || 'https://firewall.gg/assets/logo.png';
-        let request;
-        try {
-            request = await axios({
-                method: database.method,
-                url: `${database.requestUrl}${userId}`,
-                headers: {
-                    'Authorization': `Bearer r1O.CJZ9LVtLKEs2ocpZ0sR1C3436H`
-                }
-            });
-        } catch(e) {
-            if(e.toString().includes('Request failed')) {
-                request = "failed";
-            };
-        };
-        let _json = "STRING";
-        if(request != "failed") {
-            if(!request?.data) return;
-            let data = request.data;
-            if(Array.isArray(data)) {
-                // Stronger Together Array Check
-                data = await data.find(o => o.active == 1);
-            };
-            if(typeof data?.time != 'undefined') {
-                let date = new Date(data.time*1000);
-                if(!date.toString().toLowerCase().includes('invalid date')) {
-                    data.time = Number(data.time);
-                    date = new Date(data.time*1000);
-                    if(typeof data.time == 'number') {
-                        data.time = 'Unavailable.'
+        if(database.active) {
+            let themeColor = database.themeColor || '#FFFFFF';
+            let logoUrl = database.logoUrl || 'https://firewall.gg/assets/logo.png';
+            let request;
+            try {
+                request = await axios({
+                    method: database.method,
+                    url: `${database.requestUrl}${userId}`,
+                    headers: {
+                        'Authorization': `Bearer r1O.CJZ9LVtLKEs2ocpZ0sR1C3436H`
                     }
+                });
+            } catch(e) {
+                if(e.toString().includes('Request failed')) {
+                    request = "failed";
                 };
             };
-            if(data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active) {
-                if(typeof data?.active == 'number' || typeof data?.blacklistdata?.blacklisted == 'number' || typeof data?.blacklistdata?.active == 'number') {
-                    if(typeof data?.active != 'undefined') {
-                        data.active = await booleanConvert(data?.active) || data?.active;
-                    };
-                    if(typeof data?.blacklistdata?.blacklisted != 'undefined') {
-                        data.blacklistdata.blacklisted = await booleanConvert(data?.blacklistdata?.blacklisted) || data?.blacklistdata?.blacklisted;
-                    };
-                    if(typeof data?.blacklistdata?.active != 'undefined') {
-                        data.blacklistdata.active = await booleanConvert(data?.blacklistdata?.active) || data?.blacklistdata?.active;
-                    };
+            let _json = "STRING";
+            if(request != "failed") {
+                if(!request?.data) return;
+                let data = request.data;
+                if(Array.isArray(data)) {
+                    // Stronger Together Array Check
+                    data = await data.find(o => o.active == 1);
                 };
-                _json = {
-                    "database": database.name,
-                    "themeColor": themeColor,
-                    "logoUrl": logoUrl,
-                    "active": data?.active || data?.blacklistdata?.blacklisted || data?.blacklistdata?.active,
-                    "userid": data?.userid || data?.user?.id || data?.user || 'NA',
-                    "reason": data?.reason || data?.blacklistdata?.reason || data?.public_reason || 'NA',
-                    "proof": data?.proof || 'None provided...',
-                    "time": data?.time || data?.blacklistdata?.date || 'NA',
-                    "otherData": database.otherData || {}
+                if(data?.blacklistdata?.blacklisted) {
+                    data.active = data?.blacklistdata?.blacklisted;
+                    data.userid = data?.user?.id || data?.user;
+                    data.reason = data?.blacklistdata?.reason || data?.public_reason;
+                    data.time = data?.blacklistdata?.date;
+                };
+                if(data?.active) {
+                    if(typeof data?.active == 'number') {
+                        if(typeof data?.active != 'undefined') {
+                            if(data?.active == 1) {
+                                data.active = true;
+                            } else {
+                                data.active = false;
+                            };
+                        };
+                    };
+                    _json = {
+                        "database": database.name,
+                        "themeColor": themeColor,
+                        "logoUrl": logoUrl,
+                        "active": data?.active,
+                        "userid": data?.userid || 'NA',
+                        "reason": data?.reason || 'NA',
+                        "proof": data?.proof || 'None provided...',
+                        "time": data?.time || 'NA',
+                        "otherData": database.otherData || {}
+                    };
                 };
             };
-        }
-        if(typeof _json == 'object') {
-            await bans.push(_json);
+            if(typeof _json == 'object') {
+                await bans.push(_json);
+            };
+            if(count >= databases.length) {
+                check = true
+            } else {
+                check = false;
+            };
         };
-        check = await lastActions(databases, count);
     };
     while(check == true) {
         return bans;
-    };
-};
-
-async function lastActions(databases, count) {
-    if(count >= databases.length) {
-        return true;
-    } else {
-        return false;
-    };
-};
-
-async function booleanConvert(number) {
-    if(number == 1) {
-        return true;
-    } else {
-        return false;
     };
 };
