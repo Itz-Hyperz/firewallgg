dae21f11e22107c749aecd89d98f6124653a73a8
Itz-Hyperz <logan@hyperz.net>
2022-07-12 14:31:08 -0700

    Delete requestLua.lua

--- examples/requestLua.lua
+++
@@ -1,7 +0,0 @@
-local userId = "USER_ID_TO_PASS"
-PerformHttpRequest("https://firewall.hyperz.net/api/checkuser/" .. userId, function(data)
-    print("Return data: " .. data)
-end, "GET", "", {
-    ["Accept"] = "application/json, text/plain, */*",
-    ["User-Agent"] = "*"
-});
