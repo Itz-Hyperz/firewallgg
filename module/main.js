764a147c919d44286476104c9725162fef50636f
Itz-Hyperz <logan@hyperz.net>
2022-12-05 08:52:21 -0800

    Update main.js

--- module/main.js
+++ module/main.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 module.exports = async function(userId) {
+    if(typeof userId == 'undefined') console.log('No userId was defined in FirewallGG NPM request.')
     let listFetch = await axios({
         method: 'get',
         url: `https://firewall.hyperz.net/api/checkuser/${userId}`
