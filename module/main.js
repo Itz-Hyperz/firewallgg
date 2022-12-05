121a700c66da293982ce7225113f61a6d65e27ec
Xolify <103285682+XolifyDev@users.noreply.github.com>
2022-12-05 12:37:14 -0500

    Added search

--- module/main.js
+++ module/main.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const ban = (userId, userTag, databaseName, banReason, banProof) => {
+const ban = async (userId, userTag, databaseName, banReason, banProof) => {
     const fetch = await axios.post('https://firewall.hyperz.net/api/postban', {
         userId: userId,
         userTag: userTag,
@@ -11,13 +11,16 @@ const ban = (userId, userTag, databaseName, banReason, banProof) => {
     
     return fetch.data;
 }
-module.exports = async function(userId) {
+
+const search = async (userId) => {
     if(typeof userId == 'undefined') console.log('No userId was defined in FirewallGG NPM request.')
     let listFetch = await axios({
         method: 'get',
         url: `https://firewall.hyperz.net/api/checkuser/${userId}`
     });
     return listFetch.data;
-}, {
-    ban
+}
+module.exports = {
+    ban,
+    search
 }
