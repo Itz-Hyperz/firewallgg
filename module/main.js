58a13551849bdaf165659e34b57c3435b418082f
Itz-Hyperz <loganfgross@gmail.com>
2022-12-05 10:10:09 -0800

    added ability to set token AND added request headers.

--- module/main.js
+++ module/main.js
@@ -1,26 +1,43 @@
 const axios = require('axios');
+let providedToken = undefined;
 
-const ban = async (userId, userTag, databaseName, banReason, banProof) => {
-    const fetch = await axios.post('https://firewall.hyperz.net/api/postban', {
-        userId: userId,
-        userTag: userTag,
-        databaseName: databaseName,
-        banReason: banReason,
-        banProof: banProof
-    })
-    
+function setToken(token) {
+    if(typeof token == 'undefined') return console.log('Please provide your API token in the setToken request function.');
+    providedToken = token;
+};
+
+async function postBan(userId, userTag, databaseName, banReason, banProof) => {
+    if(typeof providedToken == 'undefined') return console.log('FirewallGG requires a token to post a ban, use .setToken("YOUR_API_TOKEN") before utilising postBan.');
+    const fetch = await axios({
+        method: 'POST',
+        url: 'https://firewall.hyperz.net/api/postban',
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'User-Agent': '*',
+            'authorization': providedToken
+        },
+        data: {
+            "userId": userId,
+            "userTag": userTag,
+            "databaseName": databaseName,
+            "banReason": banReason,
+            "banProof": banProof
+        }
+    });
     return fetch.data;
-}
+};
 
-const search = async (userId) => {
+async function search(userId) => {
     if(typeof userId == 'undefined') console.log('No userId was defined in FirewallGG NPM request.')
     let listFetch = await axios({
         method: 'get',
         url: `https://firewall.hyperz.net/api/checkuser/${userId}`
     });
     return listFetch.data;
-}
+};
+
 module.exports = {
-    ban,
-    search
+    setToken: setToken,
+    postBan: postBan,
+    search: search
 }
