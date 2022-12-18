39542116cd6574e3cc8ec74f5d71d52c8a03fd80
Itz-Hyperz <logan@hyperz.net>
2022-12-18 14:15:56 -0800

    fixed for 2.4.5 + added bypass list

--- examples/faxstore-extension/FirewallGG.js
+++ examples/faxstore-extension/FirewallGG.js
@@ -1,15 +1,16 @@
 const config = {
-    bansBeforeBlock: 0, // How many bans a user can have before they get 
-    createAuditLogs: true, // Create an audit log when a user gets banned via the Firewall
-    logInConsole: false // Log in the terminal when a user gets banned via the Firewall
+    bansBeforeBlock: 1, // How many bans a user can have before they get banned (minimum value is 1)
+    logInConsole: true, // Log in the terminal when a user gets banned via the Firewall
+    bypassUserIds: ["YOUR_USER_ID"] // A list of User IDs that you wish to bypass the firewall
 };
 
 const axios = require('axios');
 module.exports = async function(app, con, client, faxstore) {
-    faxstore.on('createUserAccount', async function(user, service) {
+    faxstore.on('login', async function(userObject, DbUserResults) {
+        if(config.bypassUserIds.includes(DbUserResults.userId)) return;
         let request = await axios({
             method: 'get',
-            url: `https://firewall.hyperz.net/api/checkuser/${user.userId}`
+            url: `https://firewall.hyperz.net/api/checkuser/${DbUserResults.userId}`
         }).catch(function(error) {
             return "failed";
         });
@@ -19,13 +20,12 @@ module.exports = async function(app, con, client, faxstore) {
             await request.data.forEach(ban => {
                 list.push(`${ban.database}`)
             });
-            if(config.createAuditLogs) {
-                faxstore.emit('CreateAuditLog', user.userId, 'FirewallGG Ban', `User ${user.userId} is actively banned in: ${list.join(', ')}.`);
-            };
-            if(config.logInConsole) {
-                console.log(`[FirewallGG] User ${user.userId} is actively banned in: ${list.join(', ')}.`);
-            };
-            con.query(`UPDATE users SET disabled=1, banned=1, staffnotes=staffnotes+" [FirewallGG] User ${user.userId} is actively banned in: ${list.join(', ')}." WHERE userId="${user.userId}"`, async function(err, row) {
+            if(config.logInConsole) console.log(`[FirewallGG] User ${DbUserResults.userId} is actively banned in: ${list.join(', ')}.`);
+            con.query(`UPDATE users SET disabled=1, banned=1 WHERE userId="${DbUserResults.userId}"`, async function(err, row) {
+                if(err) throw err;
+            });
+            if(DbUserResults.staffnotes == null || DbUserResults.staffnotes.toLowerCase().includes('[firewallgg]')) return;
+            con.query(`UPDATE users SET staffnotes="${DbUserResults.staffnotes}\n[FirewallGG] User ${DbUserResults.userId} is actively banned in: ${list.join(', ')}." WHERE userId="${DbUserResults.userId}"`, async function(err, row) {
                 if(err) throw err;
             });
         };
