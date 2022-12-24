30804483c66f1a3acbd09f972d0f70e3f1f75c17
Paradise Solutions <119133649+Paradise-Solutions@users.noreply.github.com>
2022-12-23 20:41:38 -0500

    Update fxmanifest.lua

--- examples/FirewallGG/fxmanifest.lua
+++ examples/FirewallGG/fxmanifest.lua
@@ -1,5 +1,3 @@
-lua54 'yes'
-
 fx_version 'adamant'
 game 'gta5'
 
@@ -9,7 +7,6 @@ author 'Paradise Modifications + Hyperz Development'
 version '1.0.0'
 
 shared_script 'config.lua'
--- client_script 'resource/client.lua'
 server_script 'resource/server.lua'
 
 dependency 'Badger_Discord_API'
